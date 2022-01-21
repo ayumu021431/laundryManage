91af5289eb74b7429e5f99c32376452f02e1fa1d
ayumu021431 <ayumu_skywalker@icloud.com>
2022-01-22 04:21:15 +0900

    Initial Commit

---
+++ laundryManage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  laundryManage
+//
+//  Created by asora on 2022/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
